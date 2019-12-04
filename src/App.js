d4df7091ed341f84293c4ada34318cfafc09857c
yogamhd <yogapratamamhd@gmail.com>
2019-12-04 11:45:02 +0700

    commit again

--- src/App.js
+++ src/App.js
@@ -53,6 +53,113 @@ function App() {
 export default App;
 
 
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
+// import {Home, About, Contact, Users, NestedUser, Header, SignIn, SignUp, Todo, TodoMongoose, TodoMysql} from "./components"
+
+// function App() {
+//   return (
+//     <Router>
+//     <Header/>
+//         <Switch>
+//           <Route path="/about">
+//             <About />
+//           </Route>
+//           <Route path="/contact">
+//             <Contact />
+//           </Route>
+//           <Route path="/todo/mongo/:email">
+//             <Todo/>
+//           </Route>
+//           <Route path="/" exact={true}>
+//             <Home />
+//           </Route>
+//           <Route path="/users" exact={true}>
+//             <Users />
+//           </Route>
+//           <Route path="/users/:id">
+//             <NestedUser />
+//           </Route>
+//           <Route path="/signin">
+//             <SignIn />
+//           </Route>
+//           <Route path="/todo/mongoose">
+//             <TodoMongoose />
+//           </Route>
+//           <Route path="/todo/mysql/">
+//             <TodoMysql />
+//           </Route>
+//           <Route path="/signup">
+//             <SignUp />
+//           </Route>
+//         </Switch>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import {
+//     Home,
+//     About,
+//     Contact,
+//     NestedUser,
+//     Users,
+//     Header,
+//     SignIn,
+//     SignUp,
+//     Todo,
+//     TodoMongoose
+// } from "./components";
+
+// function App() {
+//     return (
+//         <Router>
+//             <Header />
+//             <Switch>
+//                 <Route path="/about">
+//                     <About />
+//                 </Route>
+//                 <Route path="/contact">
+//                     <Contact />
+//                 </Route>
+//                 <Route path="/todo/mongo/:email">
+//                     <Todo />
+//                 </Route>
+//                 <Route path="/" exact={true}>
+//                     <Home />
+//                 </Route>
+//                 <Route path="/users" exact={true}>
+//                     <Users />
+//                 </Route>
+//                 <Route path="/users/:id">
+//                     <NestedUser />
+//                 </Route>
+//                 <Route path="/signin">
+//                     <SignIn />
+//                 </Route>
+//                 <Route path="/todo/mongoose">
+//                     <TodoMongoose />
+//                 </Route>
+//                 <Route path="/signup">
+//                     <SignUp />
+//                 </Route>
+//             </Switch>
+//         </Router>
+//     );
+// }
+
+// export default App;
+
+
 
 // import React from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
